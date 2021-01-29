2315d6e85bd5977bf7e162e23c70f619453bac73
YOUr <YOU@EXAMPLE.COM>
2021-01-29 02:55:02 +0100

    fix script

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const { MONGODB } = require("./config");
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers/");
-const PORT = proccess.env.port || 5000;
+const PORT = proccess.env.PORT || 5000;
 const pubsub = new PubSub();
 const server = new ApolloServer({
   typeDefs,
